12eb61adb9a7a2c5e759ff1f4c1d041d6889bb81
rtik <rtik@localhost.localdomain>
2017-10-14 19:49:17 +0100

    all calculators done (not tested yet)

--- scripts.js
+++ scripts.js
@@ -84,12 +84,114 @@ $(document).ready(function() {
       }
       $('#addition-rule-result').text(returnText);
     } else if (numNumeric == 4) {
-      $('#basic-prob-result').text("Please leave the box you want to calculate blank");
+      $('#addition-rule-result').text("Please leave the box you want to calculate blank");
     } else {
       $('#addition-rule-result').text("Please enter 3 values");
     }
   })
 
+
+
+
+
+  // multiplication rule calculator
+  $('#multiplication-rule-submit').on('click', function() {
+    var and = parseFloat($('#multiplication-rule-prob-and').val());
+    var a = parseFloat($('#multiplication-rule-prob-a').val());
+    var given = parseFloat($('#multiplication-rule-prob-given').val());
+    numNumeric = (isANumber(and) ? 1 : 0) + (isANumber(a) ? 1 : 0) + (isANumber(given) ? 1 : 0);
+    // test that there is the right number of inputs
+    if (numNumeric == 2) {
+      returnText = ""
+      if (!isANumber(and)) {
+        if (!isAProb(a) || !isAProb(given)) { returnText = "Probabilities must be between 0 and 1" }
+        else {
+          returnText = "P(A and B) = " + (a * given);
+        }
+      } else if (!isANumber(a)) {
+        if (!isAProb(and) || !isAProb(given)) { returnText = "Probabilities must be between 0 and 1" }
+        else {
+          returnText = "P(A) = " + (and / given);
+          console.log(a)
+          console.log(and)
+          console.log(given)
+        }
+      } else {
+        if (!isAProb(a) || !isAProb(given)) { returnText = "Probabilities must be between 0 and 1" }
+        else {
+          returnText = "P(A|B) = " + (a / and);
+        }
+      }
+      $('#multiplication-rule-result').text(returnText);
+    } else if (numNumeric == 3) {
+      $('#multiplication-rule-result').text("Please leave the box you want to calculate blank");
+    } else {
+      $('#multiplication-rule-result').text("Please enter 2 values");
+    }
+  })
+
+
+
+
+
+
+
+
+  // complement rule calculator
+  $('#complement-rule-submit').on('click', function() {
+    var a = parseFloat($('#complement-rule-prob-a').val());
+    // test that there is the right number of inputs
+    returnText = ""
+    if (isANumber(a)) {
+      if (!isAProb(a)) { returnText = "Probabilities must be between 0 and 1" }
+      else { returnText = "P(A and B) = " + ( 1 - a ); }
+    } else { returnText = "Please enter a probability for P(A)" }
+    $('#complement-rule-result').text(returnText);
+  })
+
+
+
+
+
+
+  // bayes rule calculator
+  $('#bayes-rule-submit').on('click', function() {
+    var ab = parseFloat($('#bayes-rule-prob-ab').val());
+    var a = parseFloat($('#bayes-rule-prob-a').val());
+    var b = parseFloat($('#bayes-rule-prob-b').val());
+    var ba = parseFloat($('#bayes-rule-prob-ba').val());
+    numNumeric = (isANumber(ab) ? 1 : 0) + (isANumber(a) ? 1 : 0) + (isANumber(b) ? 1 : 0) + (isANumber(ba) ? 1 : 0);
+    // test that there is the right number of inputs
+    if (numNumeric == 3) {
+      returnText = ""
+      if (!isANumber(ab)) {
+        if (!isAProb(a) || !isAProb(b) || !isAProb(ba)) { returnText = "Probabilities must be between 0 and 1" }
+        else {
+          returnText = "P(A|B) = " + (a * ba / b);
+        }
+      } else if (!isANumber(a)) {
+        if (!isAProb(ab) || !isAProb(b) || !isAProb(ba)) { returnText = "Probabilities must be between 0 and 1" }
+        else {
+          returnText = "P(A) = " + (ab * b / ba);
+        }
+      } else if (!isANumber(b)) {
+        if (!isAProb(ab) || !isAProb(a) || !isAProb(ba)) { returnText = "Probabilities must be between 0 and 1" }
+        else {
+          returnText = "P(B) = " + (ba * a / ab);
+        }
+      } else {
+        if (!isAProb(ab) || !isAProb(a) || !isAProb(b)) { returnText = "Probabilities must be between 0 and 1" }
+        else {
+          returnText = "P(B|A) = " + (b * ab / a);
+        }
+      }
+      $('#bayes-rule-result').text(returnText);
+    } else if (numNumeric == 4) {
+      $('#bayes-rule-result').text("Please leave the box you want to calculate blank");
+    } else {
+      $('#bayes-rule-result').text("Please enter 3 values");
+    }
+  })
 
 
 })
